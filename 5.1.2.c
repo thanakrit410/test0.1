006a84fd2b74db0fedcc3ee430f63f07c92c5dc7
Thanakrit sangsuwan <191144zs456zs@gmail.com>
2022-09-30 21:55:53 +0700

    up
    sd

---
+++ 5.1.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int num1 = 200;
+float num2 = 10.337445;
+char letter = 'A';
+char message[10]="Thailand";
+main()
+{
+    printf("%d",num1);
+    printf("%c\n%s\n",letter,message);
+    printf("%.3f\n",num2);
+    printf("%f\n",num1+num2);
+    printf("%s",message);
+
+
+
+}
+
